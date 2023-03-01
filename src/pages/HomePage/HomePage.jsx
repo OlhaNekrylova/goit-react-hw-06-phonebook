2a8cc5500ef089057d46d1300f7d4dab732382e1
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-03-01 21:40:38 +0200

    change HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
-import MyContacts from "../../modules/MyContacts/MyContacts";
-import ContactForm from "../../modules/ContactForm/ContactForm";
+// import MyContacts from "../../modules/MyContacts/MyContacts";
+// import ContactForm from "../../modules/ContactForm/ContactForm";
 import styles from "./HomePage.module.css";
 
 const HomePage = ()=> {
     return (
         <div className={styles.block}>
                     <h2>Add contact</h2>
-                    <ContactForm onSubmit={handleAddContact} />
+                    {/* <ContactForm onSubmit={handleAddContact} /> */}
                 </div>
     )
 }
