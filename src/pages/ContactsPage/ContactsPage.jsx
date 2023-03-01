4d244b8fabbf9c833d5ecd9c533440900919f9d7
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-03-01 21:42:32 +0200

    change ContactPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,10 +1,10 @@
-import ContactList from "../ContactList/ContactList";
+// import ContactList from "../ContactList/ContactList";
 
 const ContactsPage = ()=> {
     return (
         <h1>Contacts page</h1>
-        {isContacts && <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact}  />}
-        {!isContacts && <p>No contacts in list</p>}
+        // {isContacts && <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact}  />}
+        // {!isContacts && <p>No contacts in list</p>}
     ) 
 }
 
